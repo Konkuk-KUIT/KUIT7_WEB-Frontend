757895d79b0e2d94a9fdec4496a5c8f4c7d94765
wooooyun <wooyunjay@gmail.com>
2026-05-20 22:18:15 +0900

    경고창 추가

--- week7/wooyun/src/pages/Store/useCartStore.ts
+++ week7/wooyun/src/pages/Store/useCartStore.ts
@@ -23,7 +23,6 @@ interface CartState {
 
   addMenu: (menu: addMenuType) => void;
   initializeMenu: () => void;
-
 }
 
 const initialState: Pick<CartState, "menus"> = {
@@ -37,19 +36,36 @@ const useCartStore = create<CartState>((set) => ({
   addMenu: (menu) => {
     set((state) => {
       const existMenu = state.menus.find((item) => item.id === menu.id);
+      const currentStoreName = state.menus[0]?.storename;
+
+      if (currentStoreName && currentStoreName !== menu.storename) {
+        const alertMessage = window.confirm(
+          "주문서에는 같은 가게만 담을 수 있어요 \n새로 담고 이전에 담은 메뉴는 삭제할까요?",
+        );
+        
+        if(!alertMessage) {
+          return state;
+        }
+
+        return {
+          menus: [{...menu, count: 1 }],
+        };
+      }
 
       if (existMenu) {
         return {
           menus: state.menus.map((item) =>
-            item.id === menu.id ? { ...item, count: item.count + 1 } : item,
+            item.id === menu.id && item.storename === menu.storename
+              ? { ...item, count: item.count + 1 }
+              : item,
           ),
         };
       }
 
-    return {
-        menus: [...state.menus, { ...menu, count: 1}],
-    }
-    })
+      return {
+        menus: [...state.menus, { ...menu, count: 1 }],
+      };
+    });
   },
 
   initializeMenu: () => {
